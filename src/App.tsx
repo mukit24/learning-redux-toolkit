61b16f0ae4d10060337df17556c44f3bd986d340
mukit24 <mukithasanpranto4@gmail.com>
2022-06-20 17:42:38 +0600

    partial_bcz_typescript

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,9 @@ function App() {
   }
 
   const reservations = useSelector((state: RootState) => state.reservations.value)
+
+  const customers = useSelector((state:RootState)=>state.customer.value)
+  
   return (
     <div className="App">
       <div className="container">
@@ -37,16 +40,7 @@ function App() {
           </div>
         </div>
         <div className="customer-food-container">
-          <div className="customer-food-card-container">
-            <p>Selena Gomez</p>
-            <div className="customer-foods-container">
-              <div className="customer-food"></div>
-              <div className="customer-food-input-container">
-                <input />
-                <button>Add</button>
-              </div>
-            </div>
-          </div>
+          
         </div>
       </div>
     </div>
